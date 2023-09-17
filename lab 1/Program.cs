2e0dccc8b967682ba4e6b5bfe224a2636126a264
Sensei <Sensei@DESKTOP-UCJI5TB>
2023-09-17 10:45:25 +0300

    Добавьте файлы проекта.

---
+++ lab 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double number1 = random.NextDouble() * 100 - 50, number2 = random.NextDouble() * 100 - 50;
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, Sum(number1, number2));
+        }
+
+        /// <summary>
+        /// функция вычисления суммы двух вещественных чисел
+        /// </summary>
+        /// <param name="number1">прибавляемое</param>
+        /// <param name="number2">прибавляющее</param>
+        /// <returns>частное</returns>
+
+        static double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
